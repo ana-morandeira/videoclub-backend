a5c096e4b61f630e7b97521fef07ee9b207ee7f0
Ana Morandeira <ana.agrodorolo@gmail.com>
2026-03-16 18:27:48 +0100

    Main redacted

--- src/main/java/org/videoclub/Main.java
+++ src/main/java/org/videoclub/Main.java
@@ -1,17 +1,38 @@
 package org.videoclub;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import org.videoclub.model.Movie;
+import org.videoclub.repository.MovieRepositoryImpl;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) {
+        MovieRepositoryImpl movieRepository = new MovieRepositoryImpl();
+
+        // --- PELÍCULAS DE ARGENTINA ---
+        Movie movie2 = new Movie(0, "El secreto de sus ojos", "Juan José Campanella", "Thriller",
+                "Un oficial busca resolver un crimen del pasado.",
+                "src/assets/images/argentina/elSecretoDeSusOjos.jpg",
+                "src/assets/images/argentina/elSecretoDeSusOjosCartel.jpg",
+                "https://youtu.be/hKa8U-8vsfU");
+
+        Movie movie3 = new Movie(0, "Nueve reinas", "Fabián Bielinsky", "Crimen, Suspenso",
+                "Dos estafadores planean un gran golpe.",
+                "src/assets/images/argentina/nueveReinas.jpg",
+                "src/assets/images/argentina/nueveReinasCartel.jpg",
+                "https://youtu.be/Awu9WonTVB0");
+
+        // --- PELÍCULAS DE MÉXICO ---
+        Movie movie9 = new Movie(0, "Biutiful", "Alejandro G. Iñárritu", "Drama",
+                "Un hombre lucha por asegurar el futuro de sus hijos.",
+                "src/assets/images/mexico/biutiful.jpg",
+                "src/assets/images/mexico/biutifulCartel.jpg",
+                "https://youtu.be/3WzJcwGA6YM");
+
+        // --- INSERTAR EN LA BASE DE DATOS ---
+        // Descomenta las líneas de abajo una a una para ir poblando tu BBDD
+        movieRepository.createMovie(movie2);
+        movieRepository.createMovie(movie3);
+        movieRepository.createMovie(movie9);
+
+        System.out.println("Inserción completada.");
     }
 }
